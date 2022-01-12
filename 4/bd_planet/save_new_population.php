bc194b52f16970e608478ce7b0a53d995cf1d8ee
fadeevdan77 <97438735+serafimkatasonov76@users.noreply.github.com>
2022-01-12 19:06:10 +0500

    asd

--- 4/bd_planet/save_new_population.php
+++ 4/bd_planet/save_new_population.php
@@ -5,7 +5,7 @@
  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b844245c408b92","a1683317", "heroku_1f01e7efa26acd8") or die ("Невозможно подключиться к серверу");
  mysqli_query($conn, 'SET NAMES cp1251'); // Тип кодировки
  // Строка запроса на добавление записи в таблицу:
- $sql_add =  $sql_add = "INSERT INTO request SET id_planet='" . $_GET['id_planet']
+ $sql_add =  $sql_add = "INSERT INTO population SET id_planet='" . $_GET['id_planet']
 ."', id_alien='".$_GET['id_alien']."', count='".$_GET['count']."'";
  mysqli_query($conn, $sql_add); // Выполнение запроса
  if (mysqli_affected_rows($conn)>0) // если нет ошибок при выполнении запроса
