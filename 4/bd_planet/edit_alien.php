f16a834e926b4250866d6a20348feb4c363c1f5b
fadeevdan77 <97438735+serafimkatasonov76@users.noreply.github.com>
2022-01-12 18:24:22 +0500

    asd

--- 4/bd_planet/edit_alien.php
+++ 4/bd_planet/edit_alien.php
@@ -8,7 +8,7 @@
 <?php
  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b374ee7921ac55","547aa47c", "heroku_1ad19f5ab79e862") or die ("Невозможно подключиться к серверу");
  mysqli_query($conn, 'SET NAMES cp1251');
- $rows=mysqli_query($conn, "SELECT * FROM service WHERE id=".$_GET['id']);
+ $rows=mysqli_query($conn, "SELECT * FROM alien WHERE id=".$_GET['id']);
  while ($st = mysqli_fetch_array($rows)) {
  $name=$st["name"];
  $info = $st['info'];
@@ -17,7 +17,7 @@ print "<form action='save_edit_service.php' metod='get'>";
 print "Название: <input name='name' size='20' type='text'
 value='".$name."'>";
 print "<br>Описание: <textarea name='info' rows='4' cols='40'>".$_GET['info']."</textarea>";
-print "<input type='hidden' name='id' value='".$_GET['id']."'>";
+print "<br><input type='hidden' name='id' value='".$_GET['id']."'>";
 print "<input type='submit' name='' value='Сохранить'>";
 print "</form>";
 print "<p><a href=\"index.php\"> Вернуться </a>";
