a5a0c7905ef70aa8afe828a045901b41eb7749af
fadeevdan77 <97438735+serafimkatasonov76@users.noreply.github.com>
2022-01-12 18:07:13 +0500

    asd

--- 4/bd_planet/delete_alien.php
+++ 4/bd_planet/delete_alien.php
@@ -1,6 +1,6 @@
 <?php
  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b844245c408b92","a1683317", "heroku_1f01e7efa26acd8") or die ("Невозможно подключиться к серверу");
- $zapros="DELETE FROM service WHERE id=" . $_GET['id'];
+ $zapros="DELETE FROM alien WHERE id=" . $_GET['id'];
  mysqli_query($conn, $zapros);
  header("Location: index.php");
  exit;
