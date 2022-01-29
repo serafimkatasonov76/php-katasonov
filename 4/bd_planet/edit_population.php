7fdcd84cfe426c6a4f48013092d7563c91d2adf4
fadeevdan77 <97974484+loginovanastya67@users.noreply.github.com>
2022-01-30 00:12:08 +0500

    asd

--- 4/bd_planet/edit_population.php
+++ 4/bd_planet/edit_population.php
@@ -6,8 +6,6 @@
 </head>
 <body>
 <?php
- date_default_timezone_set("Asia/Yekaterinburg");
-
  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","b844245c408b92","a1683317", "heroku_1f01e7efa26acd8") or die ("Невозможно подключиться к серверу");
  mysqli_query($conn, 'SET NAMES cp1251');
  $rows=mysqli_query($conn, "SELECT * FROM population WHERE id=".$_GET['id']);
