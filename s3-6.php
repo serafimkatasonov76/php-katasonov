52b4cbcee54a550154474d84561420ed7b0d0977
fadeevdan77 <97974484+loginovanastya67@users.noreply.github.com>
2022-01-30 00:23:57 +0500

    asd

--- s3-6.php
+++ s3-6.php
@@ -26,6 +26,7 @@
     for($i = count($symbols) - 1; $i > 0; $i--) {
       if(strtolower($symbols[$i - 1]) == $after && strtolower($symbols[$i]) == $before) continue;
       echo $symbols[$i - 1];
+    }
   }
   
   echo "<form method='post' action='".$_SERVER["PHP_SELF"]."'>";
@@ -49,19 +50,19 @@
   echo "Òåêñò:<br><textarea type='text' name='text18' cols='30' rows='5'></textarea><br>";
   echo "<input type='submit' name='submit18' value='Âûïîëíèòü'></form>";
   if(isset($_POST["submit18"])) {
-  $rus_upper = str_split("ÉÖÓÊÅÍÃØÙÇÕÚÔÛÂÀÏÐÎËÄÆÝß×ÑÌÈÒÜÁÞ¨");
-  $rus_lower = str_split("éöóêåíãøùçõúôûâàïðîëäæýÿ÷ñìèòüáþ¸");
-  $eng_upper = str_split("qwertyuiopasdfghjklzxcvbnm");
-  $eng_lower = str_split("QWERTYUIOPASDFGHJKLZXCVBNM");
+    $rus_upper = str_split("ÉÖÓÊÅÍÃØÙÇÕÚÔÛÂÀÏÐÎËÄÆÝß×ÑÌÈÒÜÁÞ¨");
+    $rus_lower = str_split("éöóêåíãøùçõúôûâàïðîëäæýÿ÷ñìèòüáþ¸");
+    $eng_upper = str_split("qwertyuiopasdfghjklzxcvbnm");
+    $eng_lower = str_split("QWERTYUIOPASDFGHJKLZXCVBNM");
 
-  $lower = $upper = 0;
-  foreach(str_split($_POST["text"]) as $char)
-    if(in_array($char, $rus_lower) || in_array($char, $eng_upper)) $lower++;
-    elseif(in_array($char, $rus_upper) || in_array($char, $eng_lower)) $upper++;
+    $lower = $upper = 0;
+    foreach(str_split($_POST["text"]) as $char)
+      if(in_array($char, $rus_lower) || in_array($char, $eng_upper)) $lower++;
+      elseif(in_array($char, $rus_upper) || in_array($char, $eng_lower)) $upper++;
   
-  if($lower > $upper) echo mb_convert_case($_POST["text"], MB_CASE_LOWER, "windows-1251");
-  elseif($upper > $lower) echo mb_convert_case($_POST["text"], MB_CASE_UPPER, "windows-1251");
-  else echo $_POST["text"];
+    if($lower > $upper) echo mb_convert_case($_POST["text"], MB_CASE_LOWER, "windows-1251");
+    elseif($upper > $lower) echo mb_convert_case($_POST["text"], MB_CASE_UPPER, "windows-1251");
+    else echo $_POST["text"];
   }
 
   
